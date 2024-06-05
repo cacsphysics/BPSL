6e634c5d7aa9d6df33f77f00be5e5ec4bd83c614
Carlos A. Cartagena-Sanchez <ccartagena@brynmawr.edu>
2024-06-05 18:31:32 -0500

    Organizing Files Created two folders and moved files.

--- common_cyclotron_frequency.py
+++
@@ -1,9 +0,0 @@
-import cyclotron_frequency as cf
-
-
-B = 100
-wci = cf.ion_cyclotron(B)
-wce = cf.e_cyclotron(B)
-
-print(f"Wcp = {wci:0.2f} and Wce = {wce:0.2f}")
-print(f"The ratio is {wce/wci:0.2f}")
